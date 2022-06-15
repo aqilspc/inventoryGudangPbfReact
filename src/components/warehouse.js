fe440dbb85716c0ef37844d17c875f44f3f4e599
DimasIlham30 <midxinnn@gmail.com>
2022-06-15 21:32:37 +0700

    Fix Warehouse

--- src/components/warehouse.js
+++ src/components/warehouse.js
@@ -1,8 +1,32 @@
-import React from 'react'
-import { Carousel, Container, Row, Col, Card, Button, Navbar, Nav, NavDropdown, Image } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react'
+import { Carousel, Container, Row, Col, Card, Button, Navbar, Nav, NavDropdown, Image, Table } from 'react-bootstrap';
 import { Link } from "react-router-dom"
-
 export default function Warehouse() {
+  const [gudang, setGudang] = useState([]);
+  const ambilDataGudang = () => {
+    setGudang([])
+    // const token = JSON.parse(localStorage.getItem("token"))
+    fetch("http://127.0.0.1:8000/api/gudang_all"
+
+
+    )
+      .then((res) => res.json())
+      .then((json) => {
+
+        //   dispatch(showAll());
+        // json.data.forEach((item) => {
+        setGudang(json.data)
+        // })
+        console.log(gudang)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  useEffect(() => {
+    ambilDataGudang();
+  }, []
+  )
   return (
     <div>
         <Navbar bg="light" expand="lg">
@@ -21,6 +45,31 @@ export default function Warehouse() {
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
+            <Container>
+        <Table striped>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>MAX</th>
+              <th>MIN</th>
+            </tr>
+          </thead>
+          <tbody>
+            {gudang.map((item, index) => {
+             return <tr key={index}>
+                <td>{index}</td>
+
+                <td>{item.name}</td>
+                <td>{item.max_capacity}</td>
+                <td>{item.min_capacity}</td>
+                <td></td>
+              </tr>
+            })}
+
+          </tbody>
+        </Table>
+      </Container>
     </div>
   )
 }
